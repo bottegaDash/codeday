bc99807647eeb7b49e67fdf19cd9d13e0de3c4d2
dash <dash@cs.odu.edu>
2019-03-30 07:58:39 -0400

    Added authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  SIXTY_SECONDS_IN_MINUTE = 60 
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  SIXTY_SECONDS_IN_MINUTE = 60
+
   def round_time start_time, end_time, round_by
     minutes = (end_time.to_i - start_time.to_i)/SIXTY_SECONDS_IN_MINUTE
     if minutes % round_by > 0
       minutes = ((minutes)/round_by)*round_by+round_by
     end
     minutes
+  end
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
   end
 end
--- app/controllers/task_works_controller.rb
+++ app/controllers/task_works_controller.rb
@@ -1,21 +1,17 @@
 class TaskWorksController < ApplicationController
   def edit
     @details = TaskWork.find(params[:id])
-    if @details.user_id != current_user.id
-      puts "hey you do not have permission"
-    else
-      minutes = round_time @details.time_worked, DateTime.now.utc, 15
-      @details.time_count = minute
-      respond_to do |format|
-        if @details.save
-          format.html { redirect_to @details, notice: 'time worked was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-        else
-          format.html { render :new }
-          format.json { render json: @details.errors, status: :unprocessable_entity }
-        end
+    authorize @details
+    minutes = round_time @details.time_worked, DateTime.now.utc, 15
+    @details.time_count = minutes
+    respond_to do |format|
+      if @details.save
+        format.html { redirect_to @details, notice: 'time worked was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @details.errors, status: :unprocessable_entity }
       end
     end
   end
-
 end
